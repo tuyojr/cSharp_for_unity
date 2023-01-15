3d76e023cce7748a089b373ac484ad6df62c4a1a
tuyojr <olutuyod@gmail.com>
2023-01-15 10:50:29 +0100

    first unity script

---
+++ FirstUnityScript/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a message
+/// </summary>
+
+public class PrintMessage : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// This Start method is called when the script is activated
+    /// </summary>
+    void Start()
+    {
+        Debug.Log("Hello Gamer!");        
+    }
+    
+}
