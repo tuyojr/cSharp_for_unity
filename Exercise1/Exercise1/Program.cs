a83059e96718a5d10e63d54e8c6b1d44be86d517
tuyojr <olutuyod@gmail.com>
2023-01-15 10:05:21 +0100

    Exercise 1: Writing Your First Console App

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: Tuyo Shisui");
+            Console.WriteLine("Nemesis: Danzo Sapa");
+        }
+    }
+}
